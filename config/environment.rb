52a5eeba3eb4bf05afa1d0a9856c0677c832ba04
gnydick <gnydick@genvysuse.nydick.priv>
2011-03-30 16:01:04 -0600

    Slight change, removed load balancers from fixtures

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,13 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', 
-    :source => 'http://gems.github.com'
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
